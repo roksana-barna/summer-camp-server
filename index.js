1cb5104617e1b83ddfcf3bd917f1f531c7e2b0d8
roksana-barna <roksanabarna1803@gmail.com>
2023-08-03 23:39:37 +0600

    new code

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     const usersCollection = client.db("danceDb").collection("users");
     const classCollection = client.db("classDb").collection("class");
@@ -233,7 +233,7 @@ async function run() {
     // 
     
     app.get('/class', async (req, res) => {
-      const result = await classCollection.find().toArray();
+      const result = await classCollection.find().limit(6).toArray();
       res.send(result);
     });
     app.get('/classes', async (req, res) => {
@@ -285,7 +285,7 @@ async function run() {
     })
     
     
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
